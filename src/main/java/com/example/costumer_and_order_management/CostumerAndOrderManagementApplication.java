d1fae7446e3baab106d8a3ecb75e53408a102ed2
Gustavo Rossini <gusrossini@hotmail.com>
2026-07-30 11:28:14 -0300

    initial project structure

---
+++ src/main/java/com/example/costumer_and_order_management/CostumerAndOrderManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.costumer_and_order_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostumerAndOrderManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CostumerAndOrderManagementApplication.class, args);
+	}
+
+}
