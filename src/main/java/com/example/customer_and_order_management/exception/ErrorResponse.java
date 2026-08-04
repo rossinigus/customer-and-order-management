7ca6f7b714853c270c78ce511dc6cbdf03ec5c06
Gustavo Rossini <gusrossini@hotmail.com>
2026-08-04 17:01:37 -0300

    add ErrorResponse and GlobalExceptionHandler

---
+++ src/main/java/com/example/customer_and_order_management/exception/ErrorResponse.java
@@ -0,0 +1,29 @@
+package com.example.customer_and_order_management.exception;
+
+import java.time.LocalDateTime;
+
+public class ErrorResponse {
+
+    private LocalDateTime timestamp;
+    private int status;
+    private String message;
+
+    public ErrorResponse(int status, String message) {
+        this.timestamp = LocalDateTime.now();
+        this.status = status;
+        this.message = message;
+
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public int getStatus(){
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
---
+++ src/main/java/com/example/customer_and_order_management/exception/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.example.customer_and_order_management.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgument(IllegalArgumentException ex) {
+        ErrorResponse error = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+}
